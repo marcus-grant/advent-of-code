753580058ceeafd7d899dd556ab0a189b8e0f6f1
Marcus Grant <marcusfg@gmail.com>
2023-12-10 12:22:14 +0100

    Day 10 - Init

---
+++ 10/README.md
@@ -0,0 +1,51 @@
+# Day 10 - Pipe Maze
+
+## Part One
+
+***PLACEHOLDER FOR TEXT BEFORE EXAMPLE***
+
+**For example:**
+
+```txt
+GO TO URL BELOW, COPY EXAMPLE TEXT, AND PASTE HERE
+https://adventofcode.com/2023/day/10
+```
+
+***PLACEHOLDER FOR TEXT AFTER EXAMPLE***
+
+**Your puzzle answer was `PLACEHOLDER FOR VERIFIED INPUT ANSWER`**.
+
+## Part Two
+
+***PLACEHOLDER FOR TEXT BEFORE EXAMPLE***
+
+```txt
+GO TO URL BELOW, COPY SECOND EXAMPLE TEXT, AND PASTE HERE
+https://adventofcode.com/2023/day/10
+```
+
+***PLACEHOLDER FOR TEXT AFTER EXAMPLE***
+
+Your puzzle answer was *`PLACEHOLDER FOR VERIFIED INPUT ANSWER`*.
+
+**Both parts of this puzzle are complete!
+They provide two gold stars: \*\***
+
+At this point,
+you should [return to your Advent calendar][aoc-calendar] and
+try another puzzle.
+
+If you still want to see it,
+you can [get your puzzle input][aoc-day10-input].
+
+## Links
+
+
+- [Advent of Code - 2023 - Calendar][aoc-calendar]
+- [Advent of Code - Day 10 - Pipe Maze][aoc-day10]
+- [Advent of Code - Day 10 - Input][aoc-day10-input]
+
+<!-- Hidden References -->
+[aoc-calendar]: https://adventofcode.com/2023 "Advent of Code - Year/Calendar"
+[aoc-day10]: https://adventofcode.com/2023/day/10 "Advent of Code - Day 10"
+[aoc-day10-input]: https://adventofcode.com/2023/day/10/input "Advent of Code - Day 04 - Input"
---
+++ 10/solve.py
@@ -0,0 +1,73 @@
+import attrs
+import dataclasses
+import math
+from rich import print as rprint
+from rich import console
+import rich
+import re
+import time
+from typing import Optional, List, Tuple, Dict, Union, NewType
+
+console = console.Console()
+cprint = console.print
+
+def print_solution(
+        sol: str, title: str="", style: str="bold red", justify: str="center"
+) -> None:
+    msg = '[bold green]Solution'
+    msg += f' to {title}[/bold green]' if len(title) > 0 else '[/bold green]'
+    panel = rich.panel.Panel.fit(sol, title=msg, style="bold red")
+    cprint(panel, justify=justify, new_line_start=True)
+
+def print_panel(msg: str, title: str="",
+                style: str="yellow", justify: str="center") -> None:
+    title = None if len(title) == 0 else title
+    panel = rich.panel.Panel.fit(msg, title=title, style=style)
+    cprint(panel, justify=justify, new_line_start=True)
+
+def read_input_lines(input_file_path: str) -> list[str]:
+    with open(input_file_path, "r") as f:
+        lines = f.read().splitlines()
+    return lines
+
+def part1(input_file: str) -> int:
+    lines = read_input_lines(input_file)
+
+    return 0
+
+def part2(input_file: str) -> int:
+    lines = read_input_lines(input_file)
+
+    return 0
+
+def main():
+    EXAMPLE = f"{DAY}/example.txt"
+    INPUT = f"{DAY}/input.txt"
+    TITLES = [ # Toggle these to control which parts of the code are run
+        "Part One - EXAMPLE",
+        # "Part One - INPUT",
+        # "Part Two - EXAMPLE",
+        # "Part Two - INPUT",
+    ]
+    for t in TITLES:
+        print_panel(t, style="bold green")
+        if 'one' in t.lower() or '1' in t.lower():
+            if 'example' in t.lower():
+                print_solution(f"{part1(EXAMPLE)}", title=t)
+                continue
+            print_solution(f"{part1(INPUT)}", title=t)
+            continue
+        if 'example' in t.lower():
+            print_solution(f"{part2(EXAMPLE)}", title=t)
+            continue
+        print_solution(f"{part2(INPUT)}", title=t)
+        
+
+if __name__ == "__main__":
+    DAY = "10"
+    DAY_TITLE = "Pipe Maze"
+    msg = f"[bold green]Advent of Code - Day {DAY} - {DAY_TITLE}[/bold green]" 
+    print_panel(msg ,style="bold red")
+    main()
+    msg = f"[bold red]Advent of Code - Day {DAY} - Complete![/bold red]" 
+    print_panel(msg, style="bold green")
