9b352a02c83ce1c318a9b849e133de78e4a3cd85
Marcus Grant <marcusfg@gmail.com>
2024-12-05 14:25:14 +0100

    Update day's solve template

--- 2024/templates/solve.py
+++ 2024/templates/solve.py
@@ -5,58 +5,53 @@
 # import re
 # import time
 # import numpy as np
-# from typing import Optional, List, Tuple, Dict, Union, Literal, NewType
+import pathlib
+from typing import Union, List  # , Tuple, Dict, Literal, NewType, Optional
 
 # Util imports
-from ..util.sols import cprint
-
+# from ..util.sols import cprint
 
 # Solver ID Constants
 DAY: str = "{{ day_str }}"
 TITLE: str = "{{ day_title }}"
+COMMENTS: str = """Put any extra comments for helper script here"""
+
+# Types
+PathLike = Union[str, pathlib.Path]
 
 
-def read_lines(fpath: str) -> list[str]:
+def read_lines(fpath: PathLike) -> List[str]:
     with open(fpath, "r") as f:
         lines = f.read().splitlines()
     return lines
 
 
-def part1(fpath: str) -> int:
+def part1(fpath: PathLike, debug: bool = False) -> int:
     lines = read_lines(fpath)
 
     # TODO: Implement solution here
 
-    return 0
+    return 69
 
 
-def part2(fpath: str) -> int:
+def part2(fpath: PathLike, debug: bool = False) -> int:
     lines = read_lines(fpath)
 
     # TODO: Implement solution here
 
-    return 0
+    return 420
 
 
 def run() -> None:
-    cprint(f"Day {DAY} - {TITLE}", style="bold red")
-    cprint()
-    cprint("Part 1:")
-    cprint()
-    cprint("Solution with Example Data:")
-    cprint(f"{part1('input.txt')}", style="bold green")
-    cprint()
-    cprint("Solution with Real Data:")
-    cprint(f"{part1('input.txt')}", style="bold green")
-    cprint()
-    cprint("Part 2:")
-    cprint()
-    cprint("Solution with Example Data:")
-    cprint(f"{part2('example.txt')}", style="bold green")
-    cprint()
-    cprint("Solution with Real Data:")
-    cprint(f"{part2('input.txt')}", style="bold green")
-    cprint()
+    PATH_EX = pathlib.Path(__file__).parent / "example.txt"
+    PATH_IN = pathlib.Path(__file__).parent / "input.txt"
+    print(f"\nDay {DAY} - {TITLE}\n")
+    print("\nPart 1:\n")
+    print(f"\nSolution on Example Data:\t{part1(PATH_EX, debug=True)}\n")
+    print(f"\nSolution on Real Data:\t{part1(PATH_IN, debug=True)}\n")
+    print("\nPart 2:\n")
+    print(f"\nSolution on Example Data:\t{part2(PATH_EX, debug=True)}\n")
+    print(f"\nSolution on Real Data:\t{part2(PATH_IN, debug=True)}\n")
 
 
 if __name__ == "__main__":
