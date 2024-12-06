a32e76112d3ed84962c653b0f5d634cc55236519
Marcus Grant <marcusfg@gmail.com>
2024-12-06 19:03:21 +0100

    aoc: Improve template

--- 2024/templates/solve.py
+++ 2024/templates/solve.py
@@ -3,10 +3,11 @@
 # import dataclasses
 # import math
 # import re
-# import time
 # import numpy as np
 import pathlib
+import time
 from typing import Union, List  # , Tuple, Dict, Literal, NewType, Optional
+import sys
 
 # Util imports
 # from ..util.sols import cprint
@@ -42,17 +43,58 @@ def part2(fpath: PathLike, debug: bool = False) -> int:
     return 420
 
 
-def run() -> None:
+def bgrn(s) -> str:
+    return f"\033[1;32m{s}\033[0m"
+
+
+def mgta(s) -> str:
+    return f"\033[35m{s}\033[0m"
+
+
+def blue(s) -> str:
+    return f"\033[34m{s}\033[0m"
+
+
+def run(debug: bool = False) -> None:
+    """Runs the solution for both Part 1 and Part 2, prints results with timing.
+    Args:
+        debug: If True, runs in debug mode with verbose output.
+    """
     PATH_EX = pathlib.Path(__file__).parent / "example.txt"
     PATH_IN = pathlib.Path(__file__).parent / "input.txt"
-    print(f"\nDay {DAY} - {TITLE}\n")
-    print("\nPart 1:\n")
-    print(f"\nSolution on Example Data:\t{part1(PATH_EX, debug=True)}\n")
-    print(f"\nSolution on Real Data:\t{part1(PATH_IN, debug=True)}\n")
-    print("\nPart 2:\n")
-    print(f"\nSolution on Example Data:\t{part2(PATH_EX, debug=True)}\n")
-    print(f"\nSolution on Real Data:\t{part2(PATH_IN, debug=True)}\n")
+    kw = {"debug": debug}
+
+    print(f"\n{mgta('Day')} {bgrn(DAY)} - {mgta(TITLE)}\n")
+    print(f"\n{bgrn('Part 1')}:\n")
+
+    tstart = time.time()
+    sol = part1(PATH_EX, **kw)
+    ms = f"{1000 * (time.time() - tstart):.3f}"
+    print(f"\n{mgta('Solution with Example Data:')}\t{bgrn(sol)}\n")
+    print(blue(f"Time taken (ms):\t\t{ms}\n"))
+
+    tstart = time.time()
+    sol = part1(PATH_IN, **kw)
+    ms = f"{1000 * (time.time() - tstart):.3f}"
+    print(f"\n{mgta('Solution with Real Data:')}\t{bgrn(sol)}\n")
+    print(blue(f"Time taken (ms):\t\t{ms}\n"))
+
+    print(f"\n{bgrn('Part 2')}:\n")
+
+    tstart = time.time()
+    sol = part2(PATH_EX, **kw)
+    ms = f"{1000 * (time.time() - tstart):.3f}"
+    print(f"\n{mgta('Solution with Example Data:')}\t{bgrn(sol)}\n")
+    print(blue(f"Time taken (ms):\t\t{ms}\n"))
+
+    tstart = time.time()
+    sol = part2(PATH_IN, **kw)
+    ms = f"{1000 * (time.time() - tstart):.3f}"
+    print(f"\n{mgta('Solution with Real Data:')}\t{bgrn(sol)}\n")
+    print(blue(f"Time taken (ms):\t\t{ms}\n"))
 
 
 if __name__ == "__main__":
-    run()
+    # Check for single --debug or -d flag without argparse
+    debug = any(arg in {"--debug", "-d"} for arg in sys.argv)
+    run(debug)
