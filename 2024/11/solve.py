b2eca249d2f433df870b0029d64869b93083f670
Marcus Grant <marcusfg@gmail.com>
2024-12-11 17:20:45 +0100

    11: Solve part 1

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -4,13 +4,15 @@
 # import math
 # import re
 # import numpy as np
+from collections import deque as dq
 import pathlib
 import time
-from typing import Union, List  # , Tuple, Dict, Literal, NewType, Optional
+from typing import Union, List, Deque, Tuple  # Dict, Literal, NewType, Optional
+from rich.console import Console
 import sys
 
-# Util imports
-# from ..util.sols import cprint
+cprint = Console().print
+
 
 # Solver ID Constants
 DAY: str = "11"
@@ -27,12 +29,43 @@ def read_lines(fpath: PathLike) -> List[str]:
     return lines
 
 
+def parse_stones(fpath: PathLike) -> Deque[int]:
+    stones_str = read_lines(fpath)[0]
+    stones = dq([int(s) for s in stones_str.split()])
+    return stones
+
+
+def apply_rule2(stone_int: int) -> List[int]:
+    stone_str = str(stone_int)
+    mid = len(stone_str) // 2
+    left = int(stone_str[:mid]) if stone_str[:mid] else 0
+    right = int(stone_str[mid:]) if stone_str[mid:] else 0
+    return [left, right]
+
+
+def blink(stones: Deque[int]) -> Deque[int]:
+    new_stones = dq()
+    while stones:
+        stone = stones.popleft()
+        if stone == 0:  # Rule #1
+            new_stones.append(1)
+        elif len(str(stone)) % 2 == 0:  # Rule #2
+            new_stones.extend(apply_rule2(stone))
+        else:  # Rule #3
+            new_stones.append(stone * 2024)
+    return new_stones
+
+
+# NOTE: 334341 is too high your range was one too high for blink_count
 def part1(fpath: PathLike, debug: bool = False) -> int:
-    lines = read_lines(fpath)
+    stones = parse_stones(fpath)
+    blink_count = 7 if "example" in str(fpath) else 25
+    for _ in range(blink_count):
+        if debug:
+            cprint(stones)
+        stones = blink(stones)
 
-    # TODO: Implement solution here
-
-    return 69
+    return len(stones)
 
 
 def part2(fpath: PathLike, debug: bool = False) -> int:
@@ -79,22 +112,23 @@ def run(debug: bool = False) -> None:
     print(f"\n{mgta('Solution with Real Data:')}\t{bgrn(sol)}\n")
     print(blue(f"Time taken (ms):\t\t{ms}\n"))
 
-    print(f"\n{bgrn('Part 2')}:\n")
+    # print(f"\n{bgrn('Part 2')}:\n")
 
-    tstart = time.time()
-    sol = part2(PATH_EX, **kw)
-    ms = f"{1000 * (time.time() - tstart):.3f}"
-    print(f"\n{mgta('Solution with Example Data:')}\t{bgrn(sol)}\n")
-    print(blue(f"Time taken (ms):\t\t{ms}\n"))
+    # tstart = time.time()
+    # sol = part2(PATH_EX, **kw)
+    # ms = f"{1000 * (time.time() - tstart):.3f}"
+    # print(f"\n{mgta('Solution with Example Data:')}\t{bgrn(sol)}\n")
+    # print(blue(f"Time taken (ms):\t\t{ms}\n"))
 
-    tstart = time.time()
-    sol = part2(PATH_IN, **kw)
-    ms = f"{1000 * (time.time() - tstart):.3f}"
-    print(f"\n{mgta('Solution with Real Data:')}\t{bgrn(sol)}\n")
-    print(blue(f"Time taken (ms):\t\t{ms}\n"))
+    # tstart = time.time()
+    # sol = part2(PATH_IN, **kw)
+    # ms = f"{1000 * (time.time() - tstart):.3f}"
+    # print(f"\n{mgta('Solution with Real Data:')}\t{bgrn(sol)}\n")
+    # print(blue(f"Time taken (ms):\t\t{ms}\n"))
 
 
 if __name__ == "__main__":
     # Check for single --debug or -d flag without argparse
     debug = any(arg in {"--debug", "-d"} for arg in sys.argv)
     run(debug)
+
